3dc21ebcd0b7cb13ec4c3410289e52927bf40453
YuryP12 <pankratovy@list.ru>
2022-06-22 17:44:20 +0300

    Fist commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,80 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
+  // Ваш код, проверка значений
+  min = Infinity;
+  max = -Infinity;
+  sum = 0;
+  const len = arr.length;
 
-  // Ваш код
+  for (let i = 0; i < len; i++) {
+    sum = sum + arr[i];
+
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+  } 
+
+  avg = sum / len;
+  avg = +avg.toFixed(2);
+
+  console.log(sum, len, avg, avg.toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
 
+
+
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  const len = arr.length;
+  for (let i = 0; i < len; i++) {
+    sum = sum + arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = -Infinity;
 
-  // Ваш кода
-  // for ...
+  console.log(func);
+  const len = arrOfArr.length;
+
+  for (let i = 0; i < len; i++) {
+    const result = func(arrOfArr[i]);
+
+    if (max < result) {
+      max = result;
+    }
+  }
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+
+  let max = -Infinity;
+  let min = Infinity;
+  const len = arr.length;
+
+  for (let i = 0; i < len; i++) {
+
+    if (max < arr[i]) {
+    max = arr[i];
+    }
+
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+  }
+
+  return Math.abs(max - min);
 }
