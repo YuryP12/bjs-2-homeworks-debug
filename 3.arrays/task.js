dffae47d53c11721572e349d9d2ce2ab6708f9df
YuryP12 <pankratovy@list.ru>
2022-06-24 09:49:32 +0300

    First commit variables

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,19 @@
+//Задание 1
 function compareArrays(arr1, arr2) {
   let result;
+  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
 
-  // Ваш код
-
-  return result; // boolean
+  result = arr1.length === arr2.length && arr1.every(function(value, index) { return value === arr2[index]})
+  return result;
 }
 
+//Задание2
 function advancedFilter(arr) {
   let resultArr;
+  resultArr = arr.filter(item => item > 0).filter(item => item % 3 === 0).map(item => item * 10); 
 
-  // Ваш код
-
-  return resultArr; // array
+  return resultArr;
 }
