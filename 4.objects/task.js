f09f379930528c58eb8ef207532c3380044cc4f5
YuryP12 <pankratovy@list.ru>
2022-07-02 19:52:38 +0300

    First commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,46 @@
 function Student(name, gender, age) {
-    // Ваш код
 
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+// let student1 = new Student("Tony", "male", 37);
+// let student1 = new Student("Joker", "auto", "?");
+// let student2 = new Student("Doroty", "screwdriver", 157);
+
+Student.prototype.setSubject = function(subjectName) {
+
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+
+  if (this.marks === undefined) { 
+    this.marks = [];
+  }
+  
+  this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function(...arg) {
+
+  if (this.marks === undefined) { 
+    this.marks = [];
+  }
+
+  arg.forEach(mark => this.marks.push(mark));
+}
+
+Student.prototype.getAverage = function() {
+
+  return this.marks.reduce((a, b) => (a + b)) / this.marks.length;
+}
+
+Student.prototype.exclude = function(reason) {
+
+  delete this.subject;
+  delete this.marks;
+  //reason = this.excluded;
+  this.excluded = reason;
+}
