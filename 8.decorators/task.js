68befbe70d643597ad58f7245bd413926d81b5ff
YuryP12 <pankratovy@list.ru>
2022-07-20 22:52:44 +0300

    Fist commit variables

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,60 @@
-function cachingDecoratorNew(func) {
-  // Ваш код
-}
 
+function cachingDecoratorNew(func) {
+  const cache = [];
+
+  return (...args) => {
+    const hash = args.join(',');
+    const idx = cache.findIndex((item) => item.hash === hash);
+    let result;
+
+    if (idx !== -1) {
+      result = 'Из кэша: ' + cache[idx].value;
+    } else {
+      const value = func(...args);
+      cache.push({ hash, value });
+      if (cache.length > 5) { 
+        cache.shift();
+      }
+      result = 'Вычисляем: ' + value; 
+    }
+
+    console.log(result);
+    return result;
+  }
+}
 
 function debounceDecoratorNew(func) {
-  // Ваш код
+  let flag = false;
+  let timerId;
+
+  return (...args) => {
+    clearTimeout(timerId);
+
+    if (!flag) {
+      func(...args);
+      flag = true;
+    }
+
+    timerId = setTimeout(() => flag = false, 2000);
+  }
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+  let flag = false;
+  let timerId;
+
+  const wrapper = (...args) => {
+    clearTimeout(timerId);
+
+    if (!flag) {
+      func(...args);
+      wrapper.count++;
+      flag = true;
+    }
+
+    timerId = setTimeout(() => flag = false, ms);
+  }
+
+  wrapper.count = 0;
+  return wrapper;
 }
